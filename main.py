d3bddb71c36bda16ad2daf94af79a2e6954496d8
DaniinXorchenabo <rkbcu@mail.ru>
2021-12-21 18:42:33 +0300

    Теперь при получении данных от `Arduino` отправляется сообщение, полученное от `arduino`, по `WebSocket` всем подключенным клиентам

--- main.py
+++ main.py
@@ -2,14 +2,67 @@ import asyncio
 import os
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 import uvicorn
 
 from src.init_app import init
+
+import asyncio
+import random
+import json
+
+import websockets
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+from src.rpi.arduino import main as arduino
 
 metadata = dict()
 app = FastAPI(**metadata)
 
 app = init(app)
+
+PORT = 10010
+HOST = "localhost"
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://%s:%s/tests/received_arduino");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+""" % (HOST, PORT)
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=10010, reload=True)
--- src/rpi/arduino/main.py
+++ src/rpi/arduino/main.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
 import asyncio
-from typing import Optional, Awaitable
+from typing import Optional, Awaitable, AsyncGenerator, Type, Generator
 from datetime import datetime
 from time import time, sleep
+from abc import ABC
 
 from serial import Serial
 import serial.tools.list_ports
@@ -13,7 +15,7 @@ class OutputProtocol(asyncio.protocols.Protocol):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.current_received_string = b''
-
+        self.send_data_to: Generator = None
 
     def connection_made(self, transport: serial_asyncio.SerialTransport):
         self.transport: serial_asyncio.SerialTransport = transport
@@ -22,16 +24,23 @@ class OutputProtocol(asyncio.protocols.Protocol):
         transport.write(b'Hello, World!\n')  # Write serial data via transport
 
     def data_received(self, data: bytes):
-        if data.endswith(b'\r\n'):
-            data = self.current_received_string + data
+        # print('\tdata receiving\t:\t', repr(data))
+        self.current_received_string += data
+        if self.current_received_string.endswith(b'\r\n'):
+            data = self.current_received_string
             print('data received:\t', repr(data))
             data = data.replace(b'\r\n', b'').decode('utf-8')
+            if self.send_data_to is not None:
+                self.send_data_to: Generator
+                old_data = self.send_data_to.send(data)
+                print('old_data', old_data)
+            else:
+                print('Нет обработчика')
             self.current_received_string = b''
             """
             Делать какие-то действия с пришедшими данными
             """
-        else:
-            self.current_received_string += data
+
 
         # if b'\r\n'  data:
         #     self.current_received_string = b''
@@ -50,6 +59,49 @@ class OutputProtocol(asyncio.protocols.Protocol):
         print('resume writing')
 
 
+class GetArduinoData(ABC):
+    all_objects: set['GetArduinoData'] = set()
+    get_data_generator: Generator
+    audition: ArduinoControl
+
+    def __init__(self):
+        self.data_generator = self.get_data()
+        self.__class__.all_objects.add(self)
+        self.queue = asyncio.Queue()
+
+    def data(self) -> Awaitable:
+        return self.data_generator.__anext__()
+
+    async def get_data(self):
+        while True:
+            yield await self.queue.get()
+
+    def put_in_queue(self, putting_data):
+        return self.queue.put_nowait(putting_data)
+
+    @classmethod
+    def get_params_from_arduino(cls):
+        data = 0
+        while True:
+            # await asyncio.sleep(1)
+            print("get_params_from_arduino 1:", data)
+            new_data = yield data
+            print("get_params_from_arduino 2:", new_data)
+            if new_data is not None:
+                data = new_data
+                [obj.put_in_queue(data) for obj in cls.all_objects]
+
+    def __class_getitem__(cls, item: ArduinoControl):
+        new_cls: Type[GetArduinoData] = type(
+            f'{cls.__name__}_{item.__class__.__name__}_',
+            (GetArduinoData,),
+            dict()
+        )
+        new_cls.get_data_generator = new_cls.get_params_from_arduino()
+        new_cls.audition = item
+        return new_cls
+
+
 class ArduinoControl(object):
     speed = 9600
     all_objects: list['ArduinoControl'] = []
@@ -59,25 +111,37 @@ class ArduinoControl(object):
         self.__class__.all_objects.append(self)
         self.__class__.all_objects_as_dict[name] = self
         self.port = self.get_all_serial_ports()
+        self.send_data_to: Generator = GetArduinoData[self].get_params_from_arduino()
         self.coro = serial_asyncio.create_serial_connection(
             asyncio.get_running_loop(),
             OutputProtocol,
             self.port[0],
-            baudrate=self.speed
+            baudrate=self.speed,
         )
         self._transport: Optional[serial_asyncio.SerialTransport] = None
         self._protocol: Optional[OutputProtocol] = None
 
         async def _lambda():
             self._transport, self._protocol = await self.coro
+            self._protocol.send_data_to = self.send_data_to
             return self._transport, self._protocol
 
+        async def _useless_lambda():
+            data = 0
+            while True:
+                await asyncio.sleep(1)
+                new_data = yield data
+                if new_data is not None:
+                    data = new_data
+        next(self.send_data_to)
         self._connection_task = asyncio.create_task(_lambda())
+        # asyncio.create_task(_useless_lambda(), name='test_fantom_task')
 
     @property
     async def transport(self):
         if self._transport is None:
             self._transport, self._protocol = await self._connection_task
+            self._protocol.send_data_to = self.send_data_to
         self._transport: serial_asyncio.SerialTransport
         return self._transport
 
@@ -85,6 +149,7 @@ class ArduinoControl(object):
     async def protocol(self):
         if self._protocol is None:
             self._transport, self._protocol = await self._connection_task
+            self._protocol.send_data_to = self.send_data_to
         self._protocol: OutputProtocol
         return self._protocol
 
@@ -114,9 +179,6 @@ class ArduinoControl(object):
     @classmethod
     def close_all(cls) -> Awaitable:
         return asyncio.gather([i.close() for i in cls.all_objects])
-
-
-
 
 # class Test(object):
 #

d6940355fec4f70a3b9fd53114b894b0e6ca5464
DaniinXorchenabo <rkbcu@mail.ru>
2021-12-22 16:42:56 +0300

    реализована отправка данных, пришедших  `websockets` на `arduino`.

--- src/routes/greenhouse/metrics/app.py
+++ src/routes/greenhouse/metrics/app.py
@@ -1,21 +1,49 @@
 import random
 import asyncio
 import json
-import websockets
+from typing import Any, Optional
+import enum
 
+import websockets
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from pydantic import BaseModel, Field
 
 from src.rpi.arduino import main as arduino
-
 
 app = APIRouter(prefix=(_prefix := '/metrics'))
 app_metrics = app
 
 
+class ActionsEnum(BaseModel):
+    water: Any = None
+
+
+class SetParamEnum(BaseModel):
+    led: Optional[int] = Field(None, ge=0, le=100)
+    reb_blue_led: Optional[int] = Field(None, ge=0, le=100)
+    humidity: Optional[int] = Field(None, ge=0, le=100)
+    root_humidity: Optional[int] = Field(None, ge=0, le=100)
+    temperature: Optional[int] = Field(None, ge=-10, le=70)
+    fan: Optional[bool] = None
+    root_fan: Optional[bool] = None
+
+
+class InputAndroidData(BaseModel):
+    set: Optional[SetParamEnum]
+    get: Optional[dict[str, Any]]
+    action: Optional[ActionsEnum]
+
+
+"""
+{\"set\": {\"led\": 59}}
+"""
+
+
 # _prefix not working with @app.websocket
 @app.websocket(f'{_prefix}/sensors')
 async def websocket_chat(websocket: WebSocket):
-    get_arduino_data = arduino.GetArduinoData[arduino.ArduinoControl.all_objects_as_dict['Arduino']]()
+    arduino_controller: arduino.ArduinoControl = arduino.ArduinoControl.all_objects_as_dict['Arduino']
+    get_arduino_data = arduino.GetArduinoData[arduino_controller]()
     try:
         print('подключение...')
         await websocket.accept()
@@ -25,15 +53,19 @@ async def websocket_chat(websocket: WebSocket):
             nonlocal websocket
             async for d in get_arduino_data.data_generator:
                 # print(d)
-                await websocket.send_json(d)
+                if isinstance(d, dict):
+                    await websocket.send_json(d)
                 # await asyncio.sleep(1.5)
 
         async def lambda_receive():
             nonlocal websocket
             while True:
                 try:
-                    answer = await websocket.receive()
-                    print("\n\n------------------\n", answer, "\n------------\n")
+                    answer = old_ans = await websocket.receive_json()
+                    answer = InputAndroidData(**answer)
+                    print("\n\n------------------\n", answer, old_ans, "\n------------\n")
+                    await arduino_controller.send(answer.json())
+                    # get_arduino_data
                 except json.decoder.JSONDecodeError as e:
                     print('сообщение не является json-ом', e)
                 except ValueError as e:
